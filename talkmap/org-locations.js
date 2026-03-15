eb57ee33cf956cbc1562e800d8f082d953f13b04
Ramaguru Radhakrishnan <7790256+ramagururadhakrishnan@users.noreply.github.com>
2026-03-15 13:37:15 +0530

    Location Details Updated

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,6 +1,6 @@
 var addressPoints = [
-  [
-    "Coimbatore, India",
+   [
+    "Amrita Vishwa Vidyapeetham, Coimbatore, India",
     10.9027,
     76.9006
   ],
@@ -15,13 +15,48 @@ var addressPoints = [
     76.9400
   ],
   [
+    "Subbiah Central School, Tirupur, India",
+    11.1068,
+    77.3411
+  ],
+  [
+    "Government College of Engineering, Erode, India",
+    11.3410,
+    77.7172
+  ],
+  [
     "Sri Krishna College of Engineering and Technology, Coimbatore, India", 
     11.0779, 
     77.0023
   ],
   [
+    "St. Joseph's College of Engineering and Technology, Palai, India",
+    9.7123,
+    76.7076
+  ],
+    [
+    "Tamil Nadu Agricultural University, Coimbatore, India",
+    11.0120,
+    76.9350
+  ], 
+  [
+    "ARRS Academy, Salem, India",
+    11.6643,
+    78.1460
+  ],
+  [
     "Christ University, Lavasa, Pune, India",
     18.4055,
     73.5066
+  ],
+    [
+    "Kalasalingam University, Madurai, India",
+    9.7215,
+    77.5655
+  ],
+  [
+    "Sree Saraswathi Thyagaraja College, Pollachi, India",
+    10.7366,
+    77.0161
   ]
 ];
